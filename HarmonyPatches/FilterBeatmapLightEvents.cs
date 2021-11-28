4d6568a97f5ab2f438a545824e46799be4067956
Kinsi <kinsi55@users.noreply.github.com>
2021-11-28 06:06:33 +0100

    More efficient, altho not basegame behaviour

--- HarmonyPatches/FilterBeatmapLightEvents.cs
+++ HarmonyPatches/FilterBeatmapLightEvents.cs
@@ -1,5 +1,7 @@
 ﻿using HarmonyLib;
 using System.Linq;
+using System;
+using System.Collections.Generic;
 
 namespace ImBlindedByTheLights.HarmonyPatches {
 	[HarmonyPatch(typeof(BeatmapDataTransformHelper), nameof(BeatmapDataTransformHelper.CreateTransformedBeatmapData))]
@@ -11,30 +13,34 @@ namespace ImBlindedByTheLights.HarmonyPatches {
 				return;
 
 			if(Config.Instance.disableBackLasers || Config.Instance.disableCenterLights || Config.Instance.disableRingLights || Config.Instance.disableRotatingLasers) {
-				BeatmapData copyWithoutEvents = __result.GetCopyWithoutEvents();
+				var x = (List<BeatmapEventData>)__result.beatmapEventsData;
 
-				foreach(var beatmapEventData in __result.beatmapEventsData.OrderBy(x => x.time)) {
-					switch(beatmapEventData.type) {
+				for(var i = x.Count - 1; i > 0; i--) {
+					/*
+					 * Break affects the switchcase and removes the event
+					 * Continue is for the For loop and leaves the event
+					 */
+					switch(x[i].type) {
 						case BeatmapEventType.Event0:
-							if(Config.Instance.disableBackLasers) continue;
+							if(!Config.Instance.disableBackLasers) continue;
 							break;
 						case BeatmapEventType.Event1:
-							if(Config.Instance.disableBackLasers) continue;
+							if(!Config.Instance.disableBackLasers) continue;
 							break;
 						case BeatmapEventType.Event2:
 						case BeatmapEventType.Event3:
 						case BeatmapEventType.Event12:
 						case BeatmapEventType.Event13:
-							if(Config.Instance.disableRotatingLasers) continue;
+							if(!Config.Instance.disableRotatingLasers) continue;
 							break;
 						case BeatmapEventType.Event4:
-							if(Config.Instance.disableCenterLights) continue;
+							if(!Config.Instance.disableCenterLights) continue;
 							break;
+						default:
+							continue;
 					}
-					copyWithoutEvents.AddBeatmapEventData(beatmapEventData);
+					x.RemoveAt(i);
 				}
-
-				__result = copyWithoutEvents;
 			}
 
 			endedUpWithAnyLights = false;
