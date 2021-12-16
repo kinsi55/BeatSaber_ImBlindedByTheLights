c107371a5aa2c8369934bac084234d407418ce48
Kinsi <kinsi55@users.noreply.github.com>
2021-12-16 20:08:00 +0100

    bruh

--- UI/MAN.cs
+++ UI/MAN.cs
@@ -23,6 +23,9 @@ namespace ImBlindedByTheLights.UI {
 
 				_ = IPA.Utilities.Async.UnityMainThreadTaskScheduler.Factory.StartNew(() => {
 					sponsorsText.text = desc;
+					// There is almost certainly a better way to update / correctly set the scrollbar size...
+					sponsorsText.gameObject.SetActive(false);
+					sponsorsText.gameObject.SetActive(true);
 				});
 			}).ConfigureAwait(false);
 		}
